e15f50540e7845d1e0b00ac906c54dc975132352
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-09-17 23:53:10 +0200

    Better Mobile UI also idle hides buttons

--- src/ui_elements/noteBlock.js
+++ src/ui_elements/noteBlock.js
@@ -2,6 +2,9 @@ export class NoteBlock extends HTMLElement {
     connectedCallback() {
         const text = this.innerHTML;
         const note = this.getAttribute("text") || "Note";
-        this.innerHTML = `<note class="bg-slate-900 text-gray-100 p-2.5 border-l-2 rounded"><strong>${note}:</strong> ${text}</note>`;
+        this.innerHTML = `
+            <div class="bg-slate-900/50 text-gray-100 p-2.5 border-l-2 border-blue-400 rounded">
+                <strong>${note}:</strong> ${text}
+            </div>`;
     }
 }
